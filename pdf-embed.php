ddbcaf17b35f971bb26222dd19678fb3cb1d6ea3
Geoffrey Liu <main@g-liu.com>
2020-07-14 23:49:18 -0700

    use file id to get file url

--- pdf-embed.php
+++ pdf-embed.php
@@ -1,7 +1,9 @@
 <?php
-	$filepath = get_field( 'file' );
-	if ( ! $filepath ) return;
+	$file_id = get_field( 'file' );
+	if ( ! $file_id ) return;
+
+	$file_url = wp_get_attachment_url( $file_id );
 ?>
-<object style="width:100%; height: 700px;" data="<?php echo $filepath ?>" type="application/pdf">
-    <iframe src="<?php echo $filepath ?>"></iframe>
+<object style="width:100%; height: 700px;" data="<?php echo $file_url ?>" type="application/pdf">
+    <iframe src="<?php echo $file_url ?>"></iframe>
 </object>
